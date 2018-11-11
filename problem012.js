b10d4ba7eacc3dd3abfa2b30b358954825132fd1
karolcieslik <slickq96@gmail.com>
2018-11-12 00:48:05 +0100

    Add files via upload

--- problem012.js
+++ problem012.js
@@ -1,41 +1,31 @@
-// Karol Cieślik, pgk2
+ // Karol Cieślik, pgk2
 
-function objectsDiff(o1, o2) {
-    const ordered01 = {};
-    const ordered02 = {};
-    var  keys01 = [], keys02 = [], k;
+function objectsDiff (o1,o2) {
+  var temp = [];
+  var check = false;
+  for(k in o1) {
+    for(z in o2) { 
+      if (k == z)
+        check = true;
+    }
+    if (check == true) {
+      check = false;
+    } else {
+      temp.push(k);
+    }
+  }
 
-    Object.keys(o1).sort().forEach(function(key) {
-        ordered01[key] = o1[key];
-    });
+  for(k in o2) {
+    for(z in o1) { 
+      if (k == z)
+        check = true;
+    }
+    if (check == true) {
+      check = false;
+    } else {
+      temp.push(k);
+    }
+  }
 
-    Object.keys(o2).sort().forEach(function(key) {
-        ordered02[key] = o2[key];
-      });
-  
-      for (k in ordered01) {
-        if (ordered01.hasOwnProperty(k)) {
-          keys01.push(k);
-        }
-      }
-      
-      for (k in ordered02) {
-        if (ordered02.hasOwnProperty(k)) {
-          keys02.push(k);
-        }
-      }
-
-        keys01.sort();
-        keys02.sort();
-
-    var tempDiff = [];
-    tempDiff.push(keys01.diff(keys02));
-    tempDiff.push(keys02.diff(keys01));
-    
-    return tempDiff;
-}
-
-Array.prototype.diff = function(a) {
-    return this.filter(function(i) {return a.indexOf(i) < 0;});
-};
- 
+  return temp;
+} 
